e9fa6d440755d8051ad2bb6a0e57b557216c70d8
Benjamin Mordaunt <crawford.benjamin15@gmail.com>
2022-12-23 00:56:27 +0000

    Add MODBUS CRC-16 function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::io::{Read, Seek, SeekFrom};
-use std::fs::File;
-use std::default::Default;
-use std::mem::{transmute, size_of};
+mod crc;
 
-use byteorder::{ReadBytesExt, LittleEndian};
+use crc::bios_get_crc16;
+use std::default::Default;
+use std::fs::File;
+use std::io::{Read, Seek, SeekFrom, Write};
+use std::mem::{size_of, transmute};
+
+use byteorder::{LittleEndian, ReadBytesExt};
 
 // Represents the contents of the ARM9 bootcode, as well as
 // information about its secure area.
@@ -20,7 +23,9 @@ impl ARM9Bootcode {
         let arm9off: u64 = hdr.arm9off as u64;
 
         assert!(hdr.arm9off == 0x4000);
-        nds.seek(SeekFrom::Start(arm9off)).map_err(|_| { "Seek failed on nds file." }).unwrap();
+        nds.seek(SeekFrom::Start(arm9off))
+            .map_err(|_| "Seek failed on nds file.")
+            .unwrap();
 
         let mut contents: Vec<u64> = vec![];
         for _ in 0..hdr.arm9size {
@@ -29,10 +34,10 @@ impl ARM9Bootcode {
 
         let secure_area_encrypted = contents[0] != 0xE7FFDEFFE7FFDEFF;
 
-        ARM9Bootcode { 
-            raw_data: contents, 
-            secure_area_present: hdr.arm9off >= 0x4000 && hdr.arm9off < 0x8000, 
-            secure_area_encrypted
+        ARM9Bootcode {
+            raw_data: contents,
+            secure_area_present: hdr.arm9off >= 0x4000 && hdr.arm9off < 0x8000,
+            secure_area_encrypted,
         }
     }
 }
@@ -40,27 +45,30 @@ impl ARM9Bootcode {
 #[derive(Default)]
 #[repr(C, packed(1))]
 pub struct NDSCartridgeHeader {
-    gametitle:   [u8; 12],
-    gamecode:         u32,
-    makercode:        u16,
-    unitcode:    [u8;  1],
-    encrseedsel: [u8;  1],
-    devicecaps:  [u8;  1],
-    res0:        [u8;  8],
-    ndsregion:   [u8;  1],
-    romversion:  [u8;  1],
-    autostart:   [u8;  1],
-    arm9off:          u32,
-    arm9entry:        u32,
-    arm9raddr:        u32,
-    arm9size:         u32,
+    gametitle: [u8; 12],
+    gamecode: u32,
+    makercode: u16,
+    unitcode: [u8; 1],
+    encrseedsel: [u8; 1],
+    devicecaps: [u8; 1],
+    res0: [u8; 8],
+    ndsregion: [u8; 1],
+    romversion: [u8; 1],
+    autostart: [u8; 1],
+    arm9off: u32,
+    arm9entry: u32,
+    arm9raddr: u32,
+    arm9size: u32,
 }
 
 impl NDSCartridgeHeader {
-    
     pub fn parse_nds<R: Read + Seek>(mut cart: R) -> Self {
         let mut hdr = Self::default();
-        let hdrptr = unsafe { transmute::<&mut NDSCartridgeHeader, &mut [u8; size_of::<NDSCartridgeHeader>()]>(&mut hdr) };
+        let hdrptr = unsafe {
+            transmute::<&mut NDSCartridgeHeader, &mut [u8; size_of::<NDSCartridgeHeader>()]>(
+                &mut hdr,
+            )
+        };
 
         cart.seek(SeekFrom::Start(0)).unwrap();
         cart.read_exact(hdrptr).unwrap();
@@ -74,19 +82,18 @@ pub fn blowfish_nds(v: &mut u64, kbuf: &[u32], enc: bool) {
     let mut y: u32 = (*v >> 32) as u32;
     let mut z: u32;
 
-    let iter: Box<dyn Iterator<Item = usize>> =
-        if enc {
-            Box::new(0..16)
-        } else {
-            Box::new((2..18).rev())
+    let iter: Box<dyn Iterator<Item = usize>> = if enc {
+        Box::new(0..16)
+    } else {
+        Box::new((2..18).rev())
     };
 
     for i in iter {
         z = kbuf[i] ^ x; // P-array XOR
-        x = kbuf[(0x12  + ((z >> 24) & 0xFF)) as usize];                 // S-box[0]
+        x = kbuf[(0x12 + ((z >> 24) & 0xFF)) as usize]; // S-box[0]
         x = kbuf[(0x112 + ((z >> 16) & 0xFF)) as usize].wrapping_add(x); // S-box[1]
-        x = kbuf[(0x212 + ((z >> 8)  & 0xFF)) as usize] ^ x;             // S-box[2]
-        x = kbuf[(0x312 + ((z >> 0)  & 0xFF)) as usize].wrapping_add(x); // S-box[3]
+        x = kbuf[(0x212 + ((z >> 8) & 0xFF)) as usize] ^ x; // S-box[2]
+        x = kbuf[(0x312 + ((z >> 0) & 0xFF)) as usize].wrapping_add(x); // S-box[3]
         x = y ^ x;
         y = z;
     }
@@ -102,8 +109,8 @@ pub fn blowfish_nds(v: &mut u64, kbuf: &[u32], enc: bool) {
     *v = (y as u64) | ((x as u64) << 32);
 }
 
-/* Applies a series of arbitrary manipulations on the P-array and S-boxes 
-   depending on the title key. 
+/* Applies a series of arbitrary manipulations on the P-array and S-boxes
+   depending on the title key.
 */
 pub fn apply_keycode(tk: &mut [u32; 3], kbuf: &mut [u32]) {
     // The two encrypt steps overlap each other
@@ -120,15 +127,17 @@ pub fn apply_keycode(tk: &mut [u32; 3], kbuf: &mut [u32]) {
 
     for i in (0..131).step_by(2) {
         blowfish_nds(&mut scratch, kbuf, true);
-        kbuf[i]     = unsafe { *transmute::<*const u64, *const u32>(&scratch as *const u64).offset(1) }; 
+        kbuf[i] = unsafe { *transmute::<*const u64, *const u32>(&scratch as *const u64).offset(1) };
         kbuf[i + 1] = unsafe { *transmute::<*const u64, *const u32>(&scratch as *const u64) };
     }
 }
 
 pub fn load_encr_data<R: Read + Seek>(encr_data: &mut R) -> Result<[u32; 1042], &'static str> {
     // let data_len = encr_data.seek(SeekFrom::End(0)).map_err(|_| { "Seek failed on encryption binary." })?;
-    encr_data.seek(SeekFrom::Start(0)).map_err(|_| { "Seek failed on encryption binary." })?;
-    
+    encr_data
+        .seek(SeekFrom::Start(0))
+        .map_err(|_| "Seek failed on encryption binary.")?;
+
     let mut contents: [u32; 1042] = [0; 1042];
     for i in &mut contents {
         *i = encr_data.read_u32::<LittleEndian>().unwrap();
@@ -148,18 +157,13 @@ fn main() {
 
     let mut arm9code = ARM9Bootcode::new(&mut ndsfile, &ndshdr);
 
-    let mut keycode: [u32; 3] = [
-        ndshdr.gamecode,
-        ndshdr.gamecode >> 1,
-        ndshdr.gamecode << 1,
-    ];
+    let mut keycode: [u32; 3] = [ndshdr.gamecode, ndshdr.gamecode >> 1, ndshdr.gamecode << 1];
 
-    apply_keycode(&mut keycode, &mut encr);
-    apply_keycode(&mut keycode, &mut encr);
-    blowfish_nds(&mut arm9code.raw_data[0], &encr, true);
-    apply_keycode(&mut keycode, &mut encr);
-    blowfish_nds(&mut arm9code.raw_data[0], &encr, false);
-    println!("{:#06x}", arm9code.raw_data[0]);
+    // apply_keycode(&mut keycode, &mut encr);
+    // apply_keycode(&mut keycode, &mut encr);
+    // blowfish_nds(&mut arm9code.raw_data[0], &encr, true);
+    // apply_keycode(&mut keycode, &mut encr);
+    // blowfish_nds(&mut arm9code.raw_data[0], &encr, false);
 
     // Local variables needed to store unaligned fields (from packed header)
     let gamecode = ndshdr.gamecode;
@@ -178,4 +182,15 @@ fn main() {
             println!("NOTE: ARM9 secure area is already decrypted.");
         }
     }
+
+    // Dump the ARM9 binary
+    let mut arm9outbin = File::options()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open("arm9.bin")
+        .unwrap();
+    arm9outbin
+        .write_all(unsafe { transmute::<&[u64], &[u8]>(&arm9code.raw_data[..]) })
+        .unwrap();
 }
